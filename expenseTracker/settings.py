2ceae5e090b81020a1858076aa35c13027c664b4
Porfirio-BG <pukiriq@gmail.com>
2018-09-26 16:57:28 -0500

    Setup de la página de inicio y la primera base de datos

--- expenseTracker/settings.py
+++ expenseTracker/settings.py
@@ -77,6 +77,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'et_first': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'et_first',
+        'USER': 'django',
+        'PASSWORD': 'pimpum73',
+        'HOST': '127.0.0.1',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",}
     }
 }
 
@@ -105,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
